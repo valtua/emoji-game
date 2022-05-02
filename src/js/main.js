ebd73026725c3a86f7c8fd4173ac4e0f99b02624
Adrián Rey Lomba <adrianreylomba@gmail.com>
2022-05-02 21:45:11 +0200

    Implement logic to generate a random array and card selection

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,41 @@
+let storedCards = [];
+let failures = 0;
+
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM loaded!");
+  createGame();
 });
 
 function handleCardClick() {
-  console.log("emoji");
+  // only select one card at a time
+  if (storedCards.length > 1) return;
+
+  storedCards.push(this);
+
   this.removeEventListener("click", handleCardClick);
+
+  // return if the user hasn't selected two cards
+  if (storedCards.length !== 2) return;
+
+  if (storedCards[0].textContent !== storedCards[1].textContent) {
+    setTimeout(() => {
+      storedCards[0].addEventListener("click", handleCardClick);
+      storedCards[1].addEventListener("click", handleCardClick);
+      storedCards = [];
+    }, 1000);
+
+    failures++;
+
+    console.log("Intentos:", failures);
+
+    return;
+  }
+
+  storedCards[0].classList.add("match");
+  storedCards[1].classList.add("match");
+  storedCards = [];
 }
 
 function createGame() {
-  const emojis = [
-    "😀",
-    "🫠",
-    "😋",
-    "😋",
-    "😋",
-    "😋",
-    "😋",
-    "😋",
-    "😀",
-    "🫠",
-    "😋",
-    "😋",
-    "😋",
-    "😋",
-    "😋",
-    "😋",
-  ];
-
   const h1 = document.createElement("h1");
   h1.textContent = "Emoji Game";
   document.body.appendChild(h1);
@@ -34,9 +43,11 @@ function createGame() {
   const section = document.createElement("section");
   section.classList.add("card-section");
 
+  const emojis = shuffleEmojis();
   emojis.forEach((emoji) => {
     const article = document.createElement("article");
     article.classList.add("card");
+    // article.textContent = "❔";
     article.textContent = emoji;
 
     article.addEventListener("click", handleCardClick);
@@ -47,6 +58,32 @@ function createGame() {
   document.body.appendChild(section);
 }
 
-createGame();
+function shuffleEmojis() {
+  const emojis = [
+    "🫠",
+    "😋",
+    "😀",
+    "💩",
+    "🧐",
+    "😡",
+    "🤠",
+    "🙄",
+    "🫠",
+    "😋",
+    "😀",
+    "💩",
+    "🧐",
+    "😡",
+    "🤠",
+    "🙄",
+  ];
+  const shuffledEmojis = [];
 
-//❔
+  while (emojis.length > 0) {
+    const randomIndex = Math.floor(Math.random() * emojis.length);
+    shuffledEmojis.push(emojis[randomIndex]);
+    emojis.splice(randomIndex, 1);
+  }
+
+  return shuffledEmojis;
+}
