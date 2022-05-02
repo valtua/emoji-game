cb78dab8cf1377ee525ca178972c0cf9ca838eda
João Silva <joaofrsima2000@gmail.com>
2022-05-02 18:25:57 +0200

    primeros pasos Emoji Game

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM loaded!");
 });
+
+function handleCardClick() {
+  console.log("emoji");
+  this.removeEventListener("click", handleCardClick);
+}
+
+function createGame() {
+  const emojis = ["😀", "🫠", "😋","😋","😋","😋","😋","😋","😀", "🫠", "😋","😋","😋","😋","😋","😋"];
+  const section = document.createElement("section");
+
+  emojis.forEach((emoji) => {
+    const article = document.createElement("article");
+    article.classList.add("card");
+    article.textContent = emoji;
+
+    article.addEventListener("click", handleCardClick);
+
+    section.appendChild(article);
+  });
+
+  document.body.appendChild(section);
+}
+
+createGame();
+
+//❔
