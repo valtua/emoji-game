a883dd5e8f16d8c8f501b25901c57bb5d12f7a74
Adrián Rey Lomba <adrianreylomba@gmail.com>
2022-05-10 16:39:05 +0200

    Improve previous commit

--- src/js/EmojiUtils.js
+++ src/js/EmojiUtils.js
@@ -1,27 +1,24 @@
 import { GameState } from "./GameState";
-import data from "../data/emojis.json";
 
-export const getEmojiList = () => {
-  const emojiList = [];
-
-  while (emojiList.length < GameState.emojiLimit) {
-    const randomIndex = Math.floor(Math.random() * data.length);
-    emojiList.push(data[randomIndex]);
-    data.splice(randomIndex, 1);
-  }
-
-  return emojiList.map((emoji) => emoji);
-};
-
-export const shuffleEmojis = (emojis) => {
-  const emojiList = emojis.concat(emojis);
-  const shuffledEmojis = [];
-
-  while (emojiList.length > 0) {
-    const randomIndex = Math.floor(Math.random() * emojiList.length);
-    shuffledEmojis.push(emojiList[randomIndex]);
-    emojiList.splice(randomIndex, 1);
-  }
+export const getEmojiList = (emojis) => {
+  const emojiList = randomizeEmojis(emojis, GameState.emojiLimit);
+  const duplicatedEmojis = emojiList.concat(emojiList);
+  const shuffledEmojis = randomizeEmojis(
+    duplicatedEmojis,
+    duplicatedEmojis.length
+  );
 
   return shuffledEmojis;
+};
+
+const randomizeEmojis = (emojis, limit) => {
+  const emojiList = [];
+
+  for (let i = 0; i < limit; i++) {
+    const randomIndex = Math.floor(Math.random() * emojis.length);
+    emojiList.push(emojis[randomIndex]);
+    emojis.splice(randomIndex, 1);
+  }
+
+  return emojiList;
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,11 @@
-import { getEmojiList, shuffleEmojis } from "./EmojiUtils";
+import { getEmojiList } from "./EmojiUtils";
 import { createGame } from "./GameUtils";
 import { GameState } from "./GameState";
 import { tsParticlesConfig } from "./ModalUtils";
+import emojis from "../data/emojis.json";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const emojis = getEmojiList();
-
-  GameState.emojis = shuffleEmojis(emojis);
+  GameState.emojis = getEmojiList(emojis);
   GameState.cardsLeft = GameState.emojis.length;
   tsParticlesConfig.particles.shape.character[0].value = GameState.emojis.map(
     ({ emoji }) => emoji
